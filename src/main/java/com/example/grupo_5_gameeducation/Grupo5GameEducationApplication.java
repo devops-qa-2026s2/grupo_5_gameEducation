5418f468c0060d4da5b55734aac0c76a8780b683
GaabrielFerreira <ga.ferreiraqueiroz@hotmail.com>
2026-09-01 20:08:33 -0300

    first commit

---
+++ src/main/java/com/example/grupo_5_gameeducation/Grupo5GameEducationApplication.java
@@ -0,0 +1,13 @@
+package com.example.grupo_5_gameeducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo5GameEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo5GameEducationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/grupo_5_gameeducation/Grupo5GameEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grupo_5_gameeducation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo5GameEducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
